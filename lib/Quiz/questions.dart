822f2531421e0ea51e9ca5f9dab1156d12b79a3d
irakl <iraklis2001@hotmail.com>
2024-01-20 22:35:25 +0200

    - Avatar Palette lock / unlock - Quiz UI improvements

--- lib/Quiz/questions.dart
+++ lib/Quiz/questions.dart
@@ -2,6 +2,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart' as firedatabase;
+import 'package:google_fonts/google_fonts.dart';
 
 class Questions extends StatefulWidget {
   const Questions(
@@ -22,6 +23,9 @@ class _QuestionsState extends State<Questions> {
   var index = 0;
   late int score = 0;
   bool submit = false;
+
+  final List<bool> _isSelected = [false, false, true];
+  final List<String> alphabetSelections = ["A", "B", "C"];
 
   late int playerScore;
 
@@ -50,48 +54,83 @@ class _QuestionsState extends State<Questions> {
             },
           ),
         ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Text(
-                    "Question ${index + 1} of ${widget.listOfQuestions.length}",
-                    style: const TextStyle(fontSize: 22.0)),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  widget.listOfQuestions[index]['question'].toString(),
-                  style: const TextStyle(
-                    fontSize: 20.0,
-                  ),
-                ),
-                const Padding(padding: EdgeInsets.all(10.0)),
-                TextFormField(
-                  controller: myController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      labelText: 'Enter Answer',
-                      labelStyle: const TextStyle(fontSize: 18)),
-                ),
-                MaterialButton(
-                    minWidth: 240.0,
-                    height: 30.0,
-                    color: Colors.blue,
-                    onPressed: () {
-                      if (myController.text ==
-                          widget.listOfQuestions[index]['answer'].toString()) {
-                        setState(() {
-                          index++;
-                        });
-                        //debugPrint("snapshot key-------------------> ${widget.competitionId}");
-                      }
-                    })
-              ],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text("Question ${index + 1} of ${widget.listOfQuestions.length}",
+                style: const TextStyle(fontSize: 22.0)),
+            const SizedBox(
+              height: 20,
             ),
-          ),
+            Text(
+              widget.listOfQuestions[index]['question'].toString(),
+              style: const TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(10.0)),
+            TextFormField(
+              controller: myController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  labelText: 'Enter Answer',
+                  labelStyle: const TextStyle(fontSize: 18)),
+            ),
+            SizedBox(
+              height: 200,
+            ),
+            Container(
+              // decoration: BoxDecoration(
+              //   color: Colors.green.withOpacity(0.3)
+              // ),
+              height: 300,
+              child: ListView.builder(
+                  itemCount: widget.listOfQuestions[index]['answer'].length,
+                  itemBuilder: (BuildContext context, int answerIndex) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0, left: 5.0, right: 5.0, top: 10.0),
+                      child: SizedBox(
+                        height: 60,
+                        child: ListTile(
+                          selectedTileColor: Colors.greenAccent.withOpacity(0.5),
+                          selected: _isSelected[answerIndex],
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.green, width: 1),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          title: Text(
+                            '${widget.listOfQuestions[index]['answer'][answerIndex]}',
+                            style: GoogleFonts.roboto(
+                                fontSize: 20.0, color: Colors.white),
+                          ),
+                          // subtitle: Text(
+                          //     "None",
+                          //     style: GoogleFonts.roboto(
+                          //         fontSize: 16.0,
+                          //         color: Colors.white70
+                          //     )
+                          // ),
+                          leading: Text(
+                            alphabetSelections[answerIndex],
+                            style: GoogleFonts.roboto(
+                                fontSize: 30.0, color: Colors.white),
+                          ),
+                          onTap: () {
+                            setState(() {
+                              _isSelected[0] = false;
+                              _isSelected[1] = false;
+                              _isSelected[2] = false;
+                              _isSelected[answerIndex] = true;
+                            });
+                          },
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/customization_page.dart
@@ -0,0 +1,349 @@
+import 'dart:math';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttermoji/fluttermojiCircleAvatar.dart';
+import 'package:fluttermoji/fluttermojiCustomizer.dart';
+import 'package:fluttermoji/fluttermojiFunctions.dart';
+import 'package:fluttermoji/fluttermojiSaveWidget.dart';
+import 'package:fluttermoji/fluttermojiThemeData.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'main.dart';
+//import 'package:firebase_database/firebase_database.dart' as firedatabase;
+
+String avatarData = "";
+bool lock = true;
+
+class CustomizationPage extends StatefulWidget {
+  const CustomizationPage({Key? key}) : super(key: key);
+
+  @override
+  State<CustomizationPage> createState() => _CustomizationPageState();
+}
+
+class _CustomizationPageState extends State<CustomizationPage> {
+  Future<void> _saveAvatar(BuildContext context) async {
+    final value = await FluttermojiFunctions().encodeMySVGtoString();
+    if (context.mounted && user != null) {
+      DatabaseReference ref =
+          FirebaseDatabase.instance.ref('Players/${user?.uid}');
+      await ref.update({
+        "avatar": value,
+      });
+      //var dataSnapshot = await ref.get();
+      //var userObject = dataSnapshot.value!;
+      // print(userObject);
+      // print(user?.uid as String);
+
+      setState(() {
+        avatarData = value;
+      });
+    }
+  }
+
+  Future<void> _loadAvatar(BuildContext context) async {
+    if (context.mounted && user != null) {
+      DatabaseReference ref =
+          FirebaseDatabase.instance.ref('Players/${user?.uid}/avatar');
+      var dataSnapshot = await ref.get();
+      var userObject = dataSnapshot.value!;
+      //print(userObject);
+      //print(user?.uid as String);
+
+      //final value = FluttermojiFunctions().decodeFluttermojifromString(userObject as String);
+      //print("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+      //print(userObject as String);
+
+      setState(() {
+        avatarData = userObject as String;
+      });
+    }
+  }
+
+  //firedatabase.Query dbRef = firedatabase.FirebaseDatabase.instance.ref().child('user');
+
+  @override
+  void initState() {
+    _loadAvatar(context);
+    if (user == null){
+      lock = true;
+    }
+    super.initState();
+  }
+
+  Widget circleAvatarWidget() {
+    return CircleAvatar(
+      radius: 90,
+      backgroundColor: Colors.white70.withOpacity(0.9),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            90,
+          ),
+        ),
+        child: SvgPicture.string(
+          FluttermojiFunctions().decodeFluttermojifromString(avatarData),
+          height: 150,
+          width: 100,
+        ),
+      ),
+    );
+    // FluttermojiCircleAvatar(
+    //   backgroundColor: Colors.grey[200],
+    //   radius: 100,
+    // ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Stack(children: [
+      Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FluttermojiCircleAvatar(
+                    backgroundColor: Colors.white70.withOpacity(0.25),
+                    radius: 70,
+                  ),
+                  const Icon(Icons.arrow_right, size: 25),
+                  circleAvatarWidget(),
+                ],
+              ),
+              SizedBox(
+                width: min(600, width * 0.85),
+                child: Row(
+                  children: [
+                    Text(
+                      "Customize:",
+                      // style: Theme.of(context).textTheme.titleLarge,
+                      style: GoogleFonts.roboto(fontSize: 20.0),
+                    ),
+                    const Spacer(),
+                    // FluttermojiSaveWidget(
+                    //   onTap: ()  {
+                    //     _saveAvatar(context);
+                    //   },
+                    // ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 5.0, vertical: 20),
+                child: FluttermojiCustomizer(
+                  scaffoldWidth: min(620, width * 0.90),
+                  autosave: false,
+                  theme: FluttermojiThemeData(
+                      boxDecoration:
+                          const BoxDecoration(boxShadow: [BoxShadow()])),
+                ),
+              ),
+              Container(
+                width: 110,
+                height: 40,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: const BorderRadius.all(Radius.circular(10))),
+                child: FluttermojiSaveWidget(
+                  onTap: () {
+                    _saveAvatar(context);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: Icon(
+                          Icons.save,
+                          size: 25,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 3.0,
+                      ),
+                      Text(
+                        "Save",
+                        style: GoogleFonts.roboto(fontSize: 25.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      Visibility(
+        visible: lock,
+        child: FractionallySizedBox(
+            alignment: Alignment.center,
+            widthFactor: 1.0,
+            heightFactor: 1.0,
+            child: DecoratedBox(
+              decoration: BoxDecoration(color: Colors.black.withOpacity(0.9)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const Icon(
+                    Icons.lock,
+                    size: 70,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (user != null) {
+                        Alert(
+                          context: context,
+                          style: AlertStyle(
+                              backgroundColor: Colors.black,
+                              animationDuration:
+                                  const Duration(milliseconds: 300),
+                              animationType: AnimationType.fromBottom,
+                              descStyle:
+                                  GoogleFonts.roboto(color: Colors.white)),
+                          image: const Icon(
+                            Icons.lock_open,
+                            color: Colors.blue,
+                            size: 75,
+                          ),
+                          desc:
+                              "Would you like Avatar Customization for 500 points?",
+                          buttons: [
+                            DialogButton(
+                              onPressed: () => Navigator.pop(context),
+                              width: 120,
+                              color: Colors.red,
+                              child: Text(
+                                "No",
+                                style: GoogleFonts.roboto(
+                                    color: Colors.white, fontSize: 25),
+                              ),
+                            ),
+                            DialogButton(
+                              onPressed: () {
+                                setState(() {
+                                  lock = false;
+                                  Navigator.pop(context);
+                                });
+                              },
+                              width: 120,
+                              color: Colors.blue,
+                              child: Text(
+                                "Yes",
+                                style: GoogleFonts.roboto(
+                                    color: Colors.white, fontSize: 25),
+                              ),
+                            ),
+                          ],
+                        ).show();
+                      } else {
+                        Alert(
+                          context: context,
+                          style: AlertStyle(
+                              backgroundColor: Colors.black,
+                              animationDuration:
+                                  const Duration(milliseconds: 300),
+                              animationType: AnimationType.fromBottom,
+                              descStyle:
+                                  GoogleFonts.roboto(color: Colors.white)),
+                          image: const Icon(
+                            Icons.info_outline,
+                            color: Colors.red,
+                            size: 75,
+                          ),
+                          desc:
+                              "You need to Log In and earn 500 points to unlock this feature",
+                          buttons: [
+                            DialogButton(
+                              onPressed: () => Navigator.pop(context),
+                              width: 120,
+                              color: Colors.red,
+                              child: Text(
+                                "OK",
+                                style: GoogleFonts.roboto(
+                                    color: Colors.white, fontSize: 25),
+                              ),
+                            ),
+                          ],
+                        ).show();
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                    child: Text(
+                      "Unlock",
+                      style: GoogleFonts.roboto(fontSize: 25.0),
+                    ),
+                  )
+                ],
+              ),
+            )),
+      ),
+    ]);
+  }
+}
+// class NewPage extends StatelessWidget {
+//   const NewPage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     var _width = MediaQuery.of(context).size.width;
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Center(
+//         child: SingleChildScrollView(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               Padding(
+//                 padding: const EdgeInsets.symmetric(vertical: 30),
+//                 child: FluttermojiCircleAvatar(
+//                   radius: 100,
+//                   backgroundColor: Colors.grey[200],
+//                 ),
+//               ),
+//               SizedBox(
+//                 width: min(600, _width * 0.85),
+//                 child: Row(
+//                   children: [
+//                     Text(
+//                       "Customize:",
+//                       style: Theme.of(context).textTheme.titleLarge,
+//                     ),
+//                     Spacer(),
+//                     FluttermojiSaveWidget(),
+//                   ],
+//                 ),
+//               ),
+//               Padding(
+//                 padding:
+//                 const EdgeInsets.symmetric(horizontal: 8.0, vertical: 30),
+//                 child: FluttermojiCustomizer(
+//                   scaffoldWidth: min(600, _width * 0.85),
+//                   autosave: false,
+//                   theme: FluttermojiThemeData(
+//                       boxDecoration: BoxDecoration(boxShadow: [BoxShadow()])),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
