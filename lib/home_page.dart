94c8114c9e44dd592321ec5acdc0f8fc29e9b763
irakl <iraklis2001@hotmail.com>
2024-02-18 13:52:32 +0200

    - Pool selection improved

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -66,9 +66,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   void initState() {
     _loadGamemodes(context);
-    // print("dsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
-    // print(googleUser);
-    // print(user);
     super.initState();
   }
 
