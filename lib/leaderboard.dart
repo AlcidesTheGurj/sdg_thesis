27eb63cd942eeddd45a2da286c5b39ba7f9b3d62
irakl <iraklis2001@hotmail.com>
2024-02-19 18:23:30 +0200

    - Added basic pdf certificate

--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'mobile.dart';
 import 'auth.dart';
 import 'main.dart';
 
@@ -33,10 +34,14 @@ class _LeaderBoardState extends State<LeaderBoard> {
             width: 100,
             height: 100,
             color: _changeColor ? Colors.red : Colors.green,
-            child: ElevatedButton(onPressed: (){}, child: const Text("asd")),
-
+            child: ElevatedButton(onPressed: () {}, child: const Text("asd")),
           ),
         ),
+        ElevatedButton(
+            onPressed: () async {
+              _createPDF();
+            },
+            child: Text("generate pdf")),
       ],
     );
   }
@@ -70,4 +75,26 @@ class _LeaderBoardState extends State<LeaderBoard> {
       });
     }
   }
+}
+
+Future<void> _createPDF() async {
+  PdfDocument document = PdfDocument();
+  final page = document.pages.add();
+
+  //page.graphics.drawString('This is a test certificate if it works gg ez', PdfStandardFont(PdfFontFamily.courier, 26));
+// Add title
+  page.graphics.drawString('Certificate of Achievement',
+      PdfStandardFont(PdfFontFamily.helvetica, 30),
+      bounds: Rect.fromLTWH(50, 50, 500, 50));
+
+  // Add text
+  page.graphics.drawString(
+      'This is to certify that [Student Name] has successfully completed the quiz on SDGs.',
+      PdfStandardFont(PdfFontFamily.helvetica, 16),
+      bounds: Rect.fromLTWH(50, 120, 500, 200));
+
+  List<int> bytes = await document.save();
+  document.dispose();
+
+  saveAndLaunchFile(bytes, 'Certificate.pdf');
 }
